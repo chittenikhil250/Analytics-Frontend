a56fe2d9c47bc5b4ab745bd29800ca967652c966
Nikhil Chitte <chnikhil250@gmail.com>
2024-01-06 14:05:14 +0530

    project structure init

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import React from 'react'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from './Pages/Login';
+import Signup from './Pages/Signup';
+
 
 const App = () => {
   return (
     <>
-    
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={Login}></Route>
+        <Route path='/login' element={Login}></Route>
+        <Route path='/signup' element={Signup}></Route>
+      </Routes>    
+    </BrowserRouter>
     </>
   )
 }
