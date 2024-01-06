d2bad2c64ca1ce0b92262d7a283d06bcf92968cb
Nikhil Chitte <chnikhil250@gmail.com>
2024-01-06 14:05:55 +0530

    project structure init

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from './Pages/Login';
 import Signup from './Pages/Signup';
-
+import Home from './Pages/Home';
 
 const App = () => {
   return (
@@ -10,6 +10,7 @@ const App = () => {
     <BrowserRouter>
       <Routes>
         <Route path='/' element={Login}></Route>
+        <Route path='/home' element={Home}></Route>
         <Route path='/login' element={Login}></Route>
         <Route path='/signup' element={Signup}></Route>
       </Routes>    
